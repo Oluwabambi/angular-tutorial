8a78f889d844a34478451651e1e103feca7b9e94
Awe_Oluwabambi <oluwabambi.awe@upperlink.ng>
2024-02-15 13:21:54 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-tutorial-15th';
+  userName: any;
+
+  newForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    age: new FormControl('', [Validators.required, Validators.pattern('[0-9]+')]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    summary: new FormControl('', [Validators.required]),
+  })
+
+  ngOnInit() {
+    console.log(this.newForm);
+    const nameVal = this.newForm.controls.name; // save the name FormControl to a variable
+    nameVal.valueChanges.subscribe(value => { //   listen for changes in the value of "name"
+      this.userName = value // assign the userName property to the changed value
+    });
+    
+  }
+
+  showAlert() {
+    Swal.fire('Method called');
+  }
+
+  onSubmit() {
+    if (this.newForm.invalid) {
+      Swal.fire({
+        title: 'Error',
+        text: 'This form is invalid',
+        icon: 'error'
+      })
+    } else {
+      console.log(this.newForm.value);
+      this.newForm.reset();
+      Swal.fire('Success', 'Data has been submitted', 'success')
+    }
+  }
 }
